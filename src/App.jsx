a017243d5dceb6f6d3ca1b4a998337a7b3cbcea3
jhantu626 <jhantubala626@gmail.com>
2025-03-18 23:35:32 +0530

    configured google Poppins fonts

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+import {fonts} from './utils/fonts';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/utils/fonts.js
@@ -0,0 +1,12 @@
+const fonts = {
+  black: 'Poppins-Black',
+  bold: 'Poppins-Bold',
+  extraBold: 'Poppins-ExtraBold',
+  light: 'Poppins-Light',
+  medium: 'Poppins-Medium',
+  regular: 'Poppins-Regular',
+  semiBold: 'Poppins-SemiBold',
+  thin: 'Poppins-Thin',
+};
+
+export {fonts};
